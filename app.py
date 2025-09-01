a6444791ddd80190b9770c94c0c26c457531c83a
johani23 <ahmad.a.johani@gmail.com>
2025-09-01 12:35:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+    from flask import Flask
 from flask_cors import CORS
-from routes.user_routes import user_bp
-from database import init_db
+
+from routes.user_routes import user_routes  # استيراد البلوبّرينت
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(user_bp, url_prefix='/users')
+# تسجيل البلوبّرينت
+app.register_blueprint(user_routes)
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True)
